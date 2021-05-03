874fc67a4847851ff15321ec4dfafeb82a521a16
ngo tien thien <ngotienthien88@gmail.com>
2021-05-03 07:26:33 +0700

    remove reset panel and set default panel

--- 204240_Java_Seminar_Dictionary/src/pkg204240_java_seminar_dictionary/JFMainMenu.java
+++ 204240_Java_Seminar_Dictionary/src/pkg204240_java_seminar_dictionary/JFMainMenu.java
@@ -51,6 +51,8 @@ public class JFMainMenu extends javax.swing.JFrame {
         }
         initComponents();
 
+        clearContenPanel();        
+        addPanelToContentPanel(JPSearch);
 
     }
 
